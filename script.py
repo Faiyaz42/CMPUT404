d208b709c814a88479ea0bed356c2ba5d0f6de1c
Faiyaz Ahmed <faiyaz1@ualberta.ca>
2021-09-17 03:47:13 -0600

    Modified script.py to print source code

--- script.py
+++ script.py
@@ -3,3 +3,7 @@ import requests
 print("requests version = ",requests.__version__)
 
 print("GET requests status code = ",requests.get("http://google.com/"))
+
+x = requests.get("https://raw.github.com/Faiyaz42/CMPUT404/main/script.py")
+print(x.text)
+
